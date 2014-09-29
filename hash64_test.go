d5717d5729a90265059a5c967c5a7439aa93e679
Hugh Emberson <hugh.emberson@gmail.com>
2014-09-29 10:01:27 +1000

    Added 64 bit variant.

---
+++ hash64_test.go
@@ -0,0 +1,71 @@
+package buzhash
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"testing"
+)
+
+// Test the rolling hash property of the buzhash
+func TestRollingHash64(t *testing.T) {
+	phrase1 := "Aenean massa. Cum sociis natoque"
+	phrase2 := "Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi"
+
+	hasher1 := NewBuzHash64(32)
+	fmt.Fprint(hasher1, phrase1)
+	p1sum := hasher1.Sum64()
+
+	hasher1.Reset()
+	found := false
+	for idx, b := range []byte(loremipsum1) {
+		ssum := hasher1.HashByte(b)
+		if (ssum == p1sum) && (idx-32 == 91) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("Could not find '%s' by its checksum %08x.", phrase1, p1sum)
+	}
+
+	hasher2 := NewBuzHash64(64)
+	fmt.Fprint(hasher2, phrase2)
+	p2sum := hasher2.Sum64()
+
+	hasher2.Reset()
+	found = false
+	for idx, b := range []byte(loremipsum2) {
+		ssum := hasher2.HashByte(b)
+		if (ssum == p2sum) && (idx-64 == 592) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Errorf("Could not find '%s' by its checksum %08x.", phrase2, p2sum)
+	}
+}
+
+func TestSum64(t *testing.T) {
+	h := NewBuzHash64(16)
+	fmt.Fprint(h, loremipsum1)
+
+	sum32 := h.Sum64()
+	sumBytes := h.Sum(nil)
+
+	if l := len(sumBytes); l != 8 {
+		t.Fatalf("h.Sum() returned slice of len %d, expected 4", l)
+	}
+
+	var sumBytesAsNum uint64
+	if err := binary.Read(bytes.NewBuffer(sumBytes), binary.LittleEndian, &sumBytesAsNum); err != nil {
+		t.Fatalf("Could not read binary number? %s", err)
+	}
+
+	if sum32 != sumBytesAsNum {
+		t.Errorf("Sum32 (%08x) and Sum (%08x) returned different sums!", sum32, sumBytesAsNum)
+	}
+}
